2867afcfe2b63c0f6b38866196c2d63d82c7d2ee
Connie Chan <conniechan.10@gmail.com>
2019-03-07 01:25:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase ();
 
 const shortGreeting = 'Welcome, ';
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
